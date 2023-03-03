504a395a60a08850496dcb3ca505a62457e0df23
Roman Tokar <rtokar.appwell@ancestry.com>
2023-03-03 18:44:22 +0200

    Formatted by prettier

--- components/elements/Book/index.tsx
+++ components/elements/Book/index.tsx
@@ -1,26 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 
 import s from './Book.module.scss';
 
 export type BookProps = {
-    id: number,
-    image: any;
+  id: number;
+  image: any;
+  link: string;
+  title: string;
+  description?: string;
+  author?: string;
+  status?: string;
+  tags?: {
     link: string;
-    title: string;
-    description?: string;
-    author?: string;
-    status?: string;
-    tags?: {
-      link: string;
-      label: string;
-    }[];
+    label: string;
+  }[];
 };
 
-const Book = ({id, image, link, title, status}: BookProps) => {
-
-    return (
+const Book = ({ id, image, link, title, status }: BookProps) => {
+  return (
     <div className={s.book}>
       <div className={s.head}>
         {status && <div className={s.status}>{status}</div>}
--- components/elements/BookInformationTable/BookInformationTable.tsx
+++ components/elements/BookInformationTable/BookInformationTable.tsx
@@ -1,52 +1,64 @@
 import React from 'react';
-import s from "./BookInformationTable.module.scss"
-import clsx from "clsx";
-import Link from "next/link";
-import {BookProps} from "@/components/modules/viewItem/BookMainSection";
+import s from './BookInformationTable.module.scss';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { BookProps } from '@/components/modules/viewItem/BookMainSection';
 
 const BookInformationTable = (book: BookProps) => {
-    return (
-        <table className={s.table}>
-            <tbody>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Автор</th>
-                <td className={clsx(s.td, s.commonTdTh)}>{book.author}</td>
-            </tr>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Перекладач</th>
-                <td className={clsx(s.td, s.commonTdTh)}>{book.translator}</td>
-            </tr>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Розділів</th>
-                <td className={clsx(s.td, s.commonTdTh)}>{book.sectionsNumber}</td>
-            </tr>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Жанр</th>
-                <td className={clsx(s.td, s.commonTdTh)}>{book.genre}</td>
-            </tr>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Теги</th>
-                <td className={clsx(s.td, s.commonTdTh)}>
-                    {book.tags && (
-                        <div className={s.tags}>
-                            <ul>
-                                {book.tags.map((tag, index) => (
-                                    <li key={index}>
-                                        <Link href={tag.link}>{tag.label}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-                </td>
-            </tr>
-            <tr>
-                <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>Фендом</th>
-                <td className={clsx(s.td, s.commonTdTh)}>{book.fendom}</td>
-            </tr>
-            </tbody>
-        </table>
-    );
+  return (
+    <table className={s.table}>
+      <tbody>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Автор
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>{book.author}</td>
+        </tr>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Перекладач
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>{book.translator}</td>
+        </tr>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Розділів
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>{book.sectionsNumber}</td>
+        </tr>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Жанр
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>{book.genre}</td>
+        </tr>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Теги
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>
+            {book.tags && (
+              <div className={s.tags}>
+                <ul>
+                  {book.tags.map((tag, index) => (
+                    <li key={index}>
+                      <Link href={tag.link}>{tag.label}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </td>
+        </tr>
+        <tr>
+          <th className={clsx(s.th, s.commonTdTh)} scope={'row'}>
+            Фендом
+          </th>
+          <td className={clsx(s.td, s.commonTdTh)}>{book.fendom}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default BookInformationTable;
